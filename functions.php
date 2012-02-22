710a85dd1095efda807cbc84bd1d563219d6202c
Jack Kelly <jlk4p@virginia.edu>
2012-02-21 22:34:11 -0500

    added function support for theme options to functions.php

--- functions.php
+++ functions.php
@@ -1,41 +1,28 @@
 <?php
+// Load up  theme options
+require_once( get_template_directory() . '/theme-options.php' );
+// Define sidebars
 if ( function_exists('register_sidebar') )
-register_sidebar(array('name'=> __('Sidebar', 'digital-signage-wordpress'),
+register_sidebar(array('name'=> __('Sidebar', 'natejones-digital-signage'),
 		  'id' => 'Sidebar',
-		  'description' => __( 'The contents of this sidebar are never displayed. DockLeft is not modifiable since it contains the date and time.','digital-signage-wordpress'),
+		  'description' => __( 'The contents of this sidebar are never displayed. DockLeft is not modifiable since it contains the clock.','natejones-digital-signage'),
           'before_widget' => '<div>',
           'after_widget' => '</div>',
           'before_title' => '',
           'after_title' => '',
 ));
-register_sidebar(array('name'=> __('DockCenter', 'digital-signage-wordpress'),
-		  'id' => 'DockCenter',
-		  'description' => __( 'Recommended use: calendar events or RSS feed','digital-signage-wordpress'),
-          'before_widget' => '<div>',
-          'after_widget' => '</div>',
-          'before_title' => '<h3>',
-          'after_title' => '</h3>',
-));
-register_sidebar(array('name' => __('DockRight', 'digital-signage-wordpress'),
+register_sidebar(array('name' => __('DockRight', 'natejones-digital-signage'),
 		  'id' => 'DockRight',
-		  'description' => __( 'Recommended use: calendar events or RSS feed','digital-signage-wordpress'),
+		  'description' => __( 'Recommended use: displaying U.Va. alerts.','natejones-digital-signage'),
           'before_widget' => '<div>',
           'after_widget' => '</div>',
-          'before_title' => '<h3>',
-          'after_title' => '</h3>',
+          'before_title' => '',
+          'after_title' => '',
 ));
-register_sidebar(array('name' => 'DockFarRight',
-		  'description' => __( 'Recommended use: calendar events or RSS feed','digital-signage-wordpress'),
-          'before_widget' => '<div>',
-          'after_widget' => '</div>',
-          'before_title' => '<h3>',
-          'after_title' => '</h3>',
-));
-remove_filter ('the_content', 'wpautop');
+remove_filter('the_content', 'wpautop');
 /**
  * wp_feed_cache_transient_lifetime defaults to 12 hours for default installed RSS feed widget; 
- * this will override and set to 5 minutes (300 represents seconds) for those using the included
- * RSS widget rather than installing another 3rd party RSS plugin.
+ * this will override and set to 5 minutes (300 represents seconds)
  */
 add_filter( 'wp_feed_cache_transient_lifetime', create_function('$a', 'return 300;') );
 function get_custom_field_value($szKey, $bPrint = false) {
